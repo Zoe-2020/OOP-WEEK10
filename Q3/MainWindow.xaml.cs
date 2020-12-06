47d65e7ccb6961cc37e86fbb8bdf3eaa40a8d092
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2020-12-06 21:55:24 +0000

    Q4

---
+++ Q3/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Q3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RollBtn_Click(object sender, RoutedEventArgs e)
+        {
+           // update();
+            update2();
+        }
+
+        public void update()
+        {
+            Random rng = new Random();
+            int diceRoll = rng.Next(1, 7);
+            rollResult.Text = diceRoll.ToString();
+        }
+
+        public async void update2()
+        {
+            Random rng = new Random();
+            int diceRoll;
+
+            for(int i = 0; i < 20; i ++)
+            {
+                diceRoll = rng.Next(1, 7);
+                rollResult.Text = diceRoll.ToString();
+                await Task.Delay(50);
+            
+            }
+          
+        }
+    }
+}
---
+++ Q4/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Q4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txtInput.Text;
+            int num;
+            bool isNum = int.TryParse(input, out num);
+
+            if (isNum)
+            {
+                MessageBox.Show("value entered is a valid number");
+                txtInput.Background = Brushes.Green;
+            }
+            else
+            {
+                MessageBox.Show("value entered is NOT a valid number");
+                txtInput.Background = Brushes.Red;
+            }
+        }
+    }
+}
